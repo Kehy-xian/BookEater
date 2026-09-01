e8e993b3c47fddbfb8303937f2a4d4d4d6cdfc1a
Kehy-xian <tptps23@gmail.com>
2026-09-01 18:48:02 +0900

    Export public play-loop service

--- src/bookeater/game/__init__.py
+++ src/bookeater/game/__init__.py
@@ -1,9 +1,11 @@
 from .evolution import EvolutionDecision, resolve_evolution
 from .presentation import PublicGrowthView, public_growth_view, generic_feed_line
 from .nutrition import GrowthNutrition, project_growth_nutrition, apply_growth_nutrition
+from .loop import FeedOutcome, ReadingFeedService, outcome_from_public_dict
 
 __all__=[
     'EvolutionDecision','resolve_evolution',
     'PublicGrowthView','public_growth_view','generic_feed_line',
     'GrowthNutrition','project_growth_nutrition','apply_growth_nutrition',
+    'FeedOutcome','ReadingFeedService','outcome_from_public_dict',
 ]
