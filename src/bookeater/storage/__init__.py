b5718c6c71c90aec3fc91db865a60ae841e67a82
Kehy-xian <tptps23@gmail.com>
2026-09-01 18:29:59 +0900

    Add local storage package

---
+++ src/bookeater/storage/__init__.py
@@ -0,0 +1,15 @@
+from .sqlite_store import (
+    FeedIdCollision,
+    RevisionConflict,
+    SQLiteGameStore,
+    StateRow,
+    StoredNote,
+)
+
+__all__ = [
+    'FeedIdCollision',
+    'RevisionConflict',
+    'SQLiteGameStore',
+    'StateRow',
+    'StoredNote',
+]
