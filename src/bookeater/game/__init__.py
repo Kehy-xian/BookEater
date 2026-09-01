5f342f72606e997776f6c03fe3f2dd2fcc95583e
Kehy-xian <tptps23@gmail.com>
2026-09-01 17:50:33 +0900

    Expose opaque growth presentation helpers

--- src/bookeater/game/__init__.py
+++ src/bookeater/game/__init__.py
@@ -1,3 +1,7 @@
 from .evolution import EvolutionDecision, resolve_evolution
+from .presentation import PublicGrowthView, public_growth_view, generic_feed_line
 
-__all__=['EvolutionDecision','resolve_evolution']
+__all__=[
+    'EvolutionDecision','resolve_evolution',
+    'PublicGrowthView','public_growth_view','generic_feed_line',
+]
