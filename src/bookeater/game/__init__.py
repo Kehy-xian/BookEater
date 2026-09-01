4552f9e867784ea87552a1969d92f3a89bc60fb4
Kehy-xian <tptps23@gmail.com>
2026-09-01 18:05:40 +0900

    Export hidden nutrition projector

--- src/bookeater/game/__init__.py
+++ src/bookeater/game/__init__.py
@@ -1,7 +1,9 @@
 from .evolution import EvolutionDecision, resolve_evolution
 from .presentation import PublicGrowthView, public_growth_view, generic_feed_line
+from .nutrition import GrowthNutrition, project_growth_nutrition, apply_growth_nutrition
 
 __all__=[
     'EvolutionDecision','resolve_evolution',
     'PublicGrowthView','public_growth_view','generic_feed_line',
+    'GrowthNutrition','project_growth_nutrition','apply_growth_nutrition',
 ]
